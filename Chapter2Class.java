fd173eed43744d18d450ff739f448a0d3887cca1
Lemuel <l.castro@klab.com>
2014-06-02 16:22:37 +0800

    added branch for Chapter2

---
+++ Chapter2Class.java
@@ -0,0 +1,6 @@
+
+
+
+public class Chapter2Class {
+
+}
--- Exercises.java
+++ Exercises.java
@@ -1,10 +1,13 @@
+/* Chapter2 */
+
 
 import java.util.*;
 
 class Exercise{	
 	
 	public static void main(String args[]){
-	
+		Chapter2Class C2 = new Chapter2Class();
+		
 	}
 	
 }
