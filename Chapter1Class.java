3db0c13ce91f48e1a8043aecbb700802979c210c
Lemuel <l.castro@klab.com>
2014-06-02 16:01:47 +0800

    added chapter1class with moonweight class

---
+++ Chapter1Class.java
@@ -0,0 +1,9 @@
+
+public class Chapter1Class {
+	
+	void MoonWeight(){
+		final float weight_kg = 60.00f;
+		System.out.println("You Weigh "+weight_kg+" kg in Earth Means Your Fat");
+		System.out.println("Dont Worry you Weigh " +(weight_kg*0.17f)+" In the Moon though");
+	}
+}
--- Exercises.java
+++ Exercises.java
@@ -7,7 +7,8 @@ import java.util.*;
 class Exercise{	
 	
 	public static void main(String args[]){
-	
+			Chapter1Class C1 = new Chapter1Class();
+			C1.MoonWeight();
 	}
 	
 	
