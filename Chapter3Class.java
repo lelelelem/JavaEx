5e53a8edcd714f53d04d1019a78d03aca28c6f99
Lemuel <l.castro@klab.com>
2014-06-02 17:03:02 +0800

    added Counting spaces problem

--- Chapter3Class.java
+++ Chapter3Class.java
@@ -1,4 +1,26 @@
+import java.io.IOException;
+
 
 public class Chapter3Class {
 
+	//gets input per char until a period is recieved
+	//number of spaces is also counted
+	int getCharInput() throws IOException{
+		
+		//this will be returned after
+		//counter to count spaces
+		int SpaceCounter= 0;
+		
+		//used for's iterator for System.in,read to update c
+		//c is initialised to a simply because we only care about '.' and spaces
+		for(char c='a';c!='.';c = (char)System.in.read()){
+			if (c ==' '){
+				SpaceCounter++;
+			}
+			
+		}
+		
+		return SpaceCounter;
+	}
+	
 }
--- Exercises.java
+++ Exercises.java
@@ -1,12 +1,14 @@
 
+import java.io.IOException;
 import java.util.*;
 
 class Exercise{	
 	
-	public static void main(String args[]){
+	public static void main(String args[]) throws IOException{
 		
 		Chapter3Class C3 = new Chapter3Class();
 		
+		System.out.print("\nNumber of Spaces: "+ C3.getCharInput());
 	}
 	
 }
