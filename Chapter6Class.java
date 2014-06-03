9de3ff844503835fffc8ae3cc0941dfc343f6251
Lemuel <l.castro@klab.com>
2014-06-03 14:52:44 +0800

    added StackProblem with not so many commentsx

--- Chapter6Class.java
+++ Chapter6Class.java
@@ -1,4 +1,49 @@
+/*comments will only be used in not so obvious things*/
+
+
+import java.io.IOException;
 
 public class Chapter6Class {
 
+		private int maxSize;
+		private int currSize=0;
+		
+		private char charStack[];
+		
+		Chapter6Class(int size){
+			
+			maxSize = size;
+			charStack = new char[size];
+			
+			System.out.println("Stack max size initialized to "+maxSize);
+		}
+		
+		void push() throws IOException{
+
+			if (currSize==maxSize){
+				System.out.println("Cant push anymore pop values to be able to push again"); 
+				//used so that an additional else wont be needed 
+				return;	
+			}
+			
+			System.out.println("Input Char to push:");
+			charStack[currSize++] = (char)System.in.read();
+			System.out.println("Current Stack size now "+currSize);
+		}
+		
+		void pop(){
+			if (currSize==-1){
+				System.out.println("Cant pop anymore push values to be able to pop again"); 
+				//used so that an additional else wont be needed 
+				return;	
+			}
+			//used in order to prevent an outofboundsexception
+			//since if currsize is 0-1 then it will be negative 1
+			currSize = currSize==0 ? 0:currSize-1;
+			
+			System.out.println("Character popped is "+ charStack[currSize]);
+			System.out.println("Current Stack size now "+currSize);
+			
+		}
+		
 }
--- Exercises.java
+++ Exercises.java
@@ -1,13 +1,34 @@
 
+import java.io.IOException;
 import java.util.*;
+
+
+
 
 class Exercise{	
 	
-	public static void main(String args[]){
+	public static void main(String args[]) throws IOException{
 		
-		Chapter6Class c6 = new Chapter6Class();
+		Scanner in = new Scanner(System.in);
+		System.out.println("Input Stack Size:");
+		int ChooseOption =0;
 		
+		Chapter6Class c6 = new Chapter6Class(in.nextInt());
 		
+		while(ChooseOption!=3){
+			
+			System.out.println("\nChoose action: \n1)push\n2)pop\n3)exit\n");
+			ChooseOption = in.nextInt();
+			
+			System.out.println("\n");
+			if (ChooseOption==1){
+				c6.push();
+			}
+			else if (ChooseOption==2){
+				c6.pop();
+			}
+			
+		}
 		
 	}
 	
