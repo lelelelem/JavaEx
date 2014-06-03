a266725a8175141abdc5ac062b1b5138e8a9425d
Lemuel <l.castro@klab.com>
2014-06-03 16:24:14 +0800

    added area problem

--- Chapter7Class.java
+++ Chapter7Class.java
@@ -1,4 +1,25 @@
 
-public class Chapter7Class {
+class Chapter7Class extends TwoDShape{
 
+	final double Pi = 3.14;
+	
+	
+	Chapter7Class(double radius) {
+		super(radius);		
+	}
+
+	double area(){
+		return Pi*(radius*radius);		 
+	}
+	
+}
+
+class TwoDShape{
+	
+	double radius;
+	
+	TwoDShape(double radius){
+		this.radius = radius;
+	}
+	
 }
--- Exercises.java
+++ Exercises.java
@@ -4,9 +4,9 @@ import java.util.*;
 class Exercise{	
 	
 	public static void main(String args[]){
-		Chapter7Class c7 = new Chapter7Class();
+		Chapter7Class c7 = new Chapter7Class(11.33);
 		
-		
+		System.out.print(c7.area());
 	}
 	
 }
