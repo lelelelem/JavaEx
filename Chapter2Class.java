4f5c0bf1efe7a4c7f06605fc1ecbd5c419d3a2fe
Lemuel <l.castro@klab.com>
2014-06-03 09:59:45 +0800

    Added 7 to prime divisibility triggers

--- Chapter2Class.java
+++ Chapter2Class.java
@@ -6,8 +6,8 @@ public class Chapter2Class {
 	void FindPrimeNumbers(){
 		
 		for(int x=2; x<=100; x++){
-			if (x!=3&&x!=5){
-				if (x%2==0||x%3==0||x%5==0){
+			if (x!=3&&x!=5&&x!=2&&x!=7){
+				if (x%2==0||x%3==0||x%5==0||x%7==0){
 					System.out.println(x+ " is a Prime Number...");
 				}	
 			}
