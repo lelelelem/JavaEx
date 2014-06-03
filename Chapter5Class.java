d59cfce0a8903c521c814d2283685301c31516ed
Lemuel <l.castro@klab.com>
2014-06-03 13:34:17 +0800

    Solved Ave to 10 problem though the program is up to 11 since array is length 11

--- Chapter5Class.java
+++ Chapter5Class.java
@@ -1,4 +1,17 @@
 
 public class Chapter5Class {
 
+	int ctr=0;
+	
+	//Arr[] is the whole array, double since we are getting average
+	//num is the length of the Array Arr[]
+	//sum is passed as 0 bec. this will be used for containing the sum
+	double Ave10(double Arr[], int num, double sum){
+		if(ctr==num)
+			return sum/num; //base case divides sum over length
+		else
+			return Ave10(Arr, num, sum+Arr[ctr++]);//recursion sum+Arr[ctr++] adds all numbers in Arr[]
+	}
+	
+	
 }
--- Exercises.java
+++ Exercises.java
@@ -4,8 +4,11 @@ import java.util.*;
 class Exercise{	
 	
 	public static void main(String args[]){
-		Chapter5Class c5 = new Chapter5Class();
 		
+		double ArrayToGetAve[] = {1,2,3,4,5,6,7,8,9,10,11}; 
+		
+		Chapter5Class c5 = new Chapter5Class();
+		System.out.print(c5.Ave10(ArrayToGetAve, ArrayToGetAve.length,0.0));
 		
 	}
 	
